79ed0af6169bfab4d86d9832e5aed27d16cf2ee1
yashas8197 <yashasjr7@gmail.com>
2024-11-01 17:57:58 +0530

    added title

--- frontend/src/components/ExerciseList.jsx
+++ frontend/src/components/ExerciseList.jsx
@@ -82,51 +82,58 @@ const ExerciseList = () => {
   };
 
   return (
-    <div className="border border-gray-400 w-full sm:w-3/4 mx-auto my-5 p-4 sm:p-5 rounded-lg">
-      <div className="flex justify-between items-center">
-        <h1 className="text-[#8CAAE7] font-bold text-lg sm:text-xl">
-          Exercise Programme
-        </h1>
-        <button
-          onClick={showComboList}
-          className="bg-[#8EA7E3] px-2 py-1 rounded-lg text-white font-semibold"
-        >
-          Show All Combos
-        </button>
-        <Button onClick={clearAllExercises} variant="destructive">
-          Clear All
-        </Button>
-      </div>
-
-      {loading ? (
-        <div className="flex justify-center items-center h-[300px] sm:h-[500px]">
-          <p className="text-xl sm:text-2xl text-[#8CAAE7]">Loading...</p>
+    <>
+      <h2 className="text-center my-4 font-bold font-serif text-2xl">
+        Empowering You To Live A Pain-Free Life
+      </h2>
+      <div className="border border-gray-400 w-full sm:w-3/4 mx-auto my-5 p-4 sm:p-5 rounded-lg">
+        <div className="flex justify-between items-center">
+          <h1 className="text-[#8CAAE7] font-bold text-lg sm:text-xl">
+            Exercise Programme
+          </h1>
+          <button
+            onClick={showComboList}
+            className="bg-[#8EA7E3] px-2 py-1 rounded-lg text-white font-semibold"
+          >
+            Show All Combos
+          </button>
+          <Button onClick={clearAllExercises} variant="destructive">
+            Clear All
+          </Button>
         </div>
-      ) : (
-        <section ref={scrollContainerRef} className="mt-4">
-          <ScrollArea className="h-[300px] sm:h-[500px]">
-            <Reorder.Group axis="y" values={category} onReorder={setCategory}>
-              {category.map((cat) => (
-                <Reorder.Item key={cat._id} value={cat} onDrag={scrollOnDrag}>
-                  <div className="flex items-center">
-                    <GrDrag className="cursor-grab mr-2" />
-                    <ExerciseCard
-                      category={cat}
-                      setCategory={setCategory}
-                      shouldDisableDuplicate={shouldDisableDuplicate(cat.name)}
-                    />
-                  </div>
-                </Reorder.Item>
-              ))}
-            </Reorder.Group>
-          </ScrollArea>
-          <div className="flex flex-col sm:flex-row justify-between items-center mt-4">
-            <AddExercise setCategory={setCategory} />
-            <CreateCombo setCategory={setCategory} category={category} />
+
+        {loading ? (
+          <div className="flex justify-center items-center h-[300px] sm:h-[500px]">
+            <p className="text-xl sm:text-2xl text-[#8CAAE7]">Loading...</p>
           </div>
-        </section>
-      )}
-    </div>
+        ) : (
+          <section ref={scrollContainerRef} className="mt-4">
+            <ScrollArea className="h-[300px] sm:h-[500px]">
+              <Reorder.Group axis="y" values={category} onReorder={setCategory}>
+                {category.map((cat) => (
+                  <Reorder.Item key={cat._id} value={cat} onDrag={scrollOnDrag}>
+                    <div className="flex items-center">
+                      <GrDrag className="cursor-grab mr-2" />
+                      <ExerciseCard
+                        category={cat}
+                        setCategory={setCategory}
+                        shouldDisableDuplicate={shouldDisableDuplicate(
+                          cat.name
+                        )}
+                      />
+                    </div>
+                  </Reorder.Item>
+                ))}
+              </Reorder.Group>
+            </ScrollArea>
+            <div className="flex flex-col sm:flex-row justify-between items-center mt-4">
+              <AddExercise setCategory={setCategory} />
+              <CreateCombo setCategory={setCategory} category={category} />
+            </div>
+          </section>
+        )}
+      </div>
+    </>
   );
 };
 
